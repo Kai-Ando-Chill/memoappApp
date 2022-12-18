87a9811ad7461a2cc693683725094d693c1da7cc
KaiAndo <71166908+Kai-Ando-Chill@users.noreply.github.com>
2022-12-18 22:22:29 +0900

    create lobin/signupScreen

---
+++ src/screens/LogInScreen.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+  View, Text, TextInput, StyleSheet,
+} from 'react-native';
+
+import Appbar from '../components/Appbar';
+import Button from '../components/Button';
+
+export default function LogInScreen() {
+  return (
+    <View style={styles.container}>
+      <Appbar />
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput style={styles.input} value="Email Adress" />
+        <TextInput style={styles.input} value="Password" />
+        <Button label="submit" />
+        <View style={styles.fotter}>
+          <Text style={styles.fotterText}>Not resistered?</Text>
+          <Text style={styles.fotterLink}>Sign up here!</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  fotterText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  fotterLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FD3',
+  },
+  fotter: {
+    flexDirection: 'row',
+  },
+});
